7557dd4e10c810956fe036966af9054ffc7f9497
shadowKayn <pakobarbakadze@gmail.com>
2022-06-09 12:30:12 +0400

    Preview

--- src/components/Projects/ProjectCards.jsx
+++ src/components/Projects/ProjectCards.jsx
@@ -6,11 +6,7 @@ const ProjectCards = ({ project }) => {
   return (
     <React.Fragment>
       <section className={classes.section}>
-        <a
-          href={project.link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href={project.link} target="_blank" rel="noopener noreferrer">
           <img src={project.image} alt="pirateit" className={classes.preview} />
         </a>
         <h1>{project.title}</h1>
